910564df9465f924383c1133559d79cb44d56e53
jhonkus <putukn@gmail.com>
2022-05-07 09:14:27 +0800

    Load .env

--- TwitterTrackerApp/Program.cs
+++ TwitterTrackerApp/Program.cs
@@ -5,6 +5,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+DotNetEnv.Env.Load();
+DotNetEnv.Env.TraversePath().Load();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
